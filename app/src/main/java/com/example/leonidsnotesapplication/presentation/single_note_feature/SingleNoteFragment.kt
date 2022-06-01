ab512c803f72ade95af39202cb1e097c59bb13fd
Leonid06 <x.worklene@gmail.com>
2022-06-01 19:07:26 +0300

    работает скроллинг заметки

--- app/src/main/java/com/example/leonidsnotesapplication/presentation/single_note_feature/SingleNoteFragment.kt
+++ app/src/main/java/com/example/leonidsnotesapplication/presentation/single_note_feature/SingleNoteFragment.kt
@@ -1,6 +1,7 @@
 package com.example.leonidsnotesapplication.presentation.single_note_feature
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
