3a6ab148f220979f6268a0a743491cae74f8a5ec
Leonid06 <x.worklene@gmail.com>
2022-06-14 11:47:16 +0300

    refactoring again

--- app/src/main/java/com/example/leonidsnotesapplication/presentation/notes_feature/NotesFragment.kt
+++ app/src/main/java/com/example/leonidsnotesapplication/presentation/notes_feature/NotesFragment.kt
@@ -5,12 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.leonidsnotesapplication.R
